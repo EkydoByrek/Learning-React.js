688d11698d567a288cf932da68cd8917709b0440
Eldar Bajric <117043992+EkydoByrek@users.noreply.github.com>
2023-01-21 20:40:48 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Da li ovo radi pls 
+        </h1>
+        <a
+          className="Creator-link"
+          href="https://www.linkedin.com/in/eldar-bajri%C4%87-5b8547255/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My linked in profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
